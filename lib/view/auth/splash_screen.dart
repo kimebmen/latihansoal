4affbc3ae1d448c9d0ad94fa2a32cd5539b984a8
kimebmen <simbolonkimebmen@gmail.com>
2022-04-08 22:44:37 +0800

    addLoginPage

--- lib/view/auth/splash_screen.dart
+++ lib/view/auth/splash_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:test1/view/login_page.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -7,10 +8,17 @@ class SplashScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       // appBar: AppBar(),
-      body: Center(
-        child: Image.asset(
-          "assets/auth/ic_splash.png",
-          width: MediaQuery.of(context).size.width * 0.5,
+      body: GestureDetector(
+        onTap: (){
+          Navigator.push(context, MaterialPageRoute(builder: (context){
+            return LoginPage();
+          }));
+        },
+        child: Center(
+          child: Image.asset(
+            "assets/auth/ic_splash.png",
+            width: MediaQuery.of(context).size.width * 0.5,
+          ),
         ),
       ),
       backgroundColor: Color(0xff3A7FD5),
--- lib/view/login_page.dart
+++ lib/view/login_page.dart
@@ -1,20 +1,65 @@
 import 'package:flutter/material.dart';
 
-class LoginScreen extends StatelessWidget {
-  const LoginScreen({ Key? key }) : super(key: key);
+class LoginPage extends StatelessWidget {
+  const LoginPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Text("Login"),
-          Image.asset("asset/auth/img_login.png"),
-          Text(""),
-          Text()
-        ],
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            children: [
+              Text("Login"),
+              Image.asset("assets/auth/img_login.png"),
+              Text(
+                "Selamat Datang",
+                style: TextStyle(
+                  fontSize: 16,
+                ),
+              ),
+              Text(
+                  "Selamat Datang di Aplikasi Widya Edu Aplikasi Latihan dan Konsultasi Soal"),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    elevation: 5,
+                    primary: Colors.white,
+                    fixedSize: const Size(double.infinity, 50),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(25),
+                      side: const BorderSide(
+                        color: Colors.blue,
+                        style: BorderStyle.solid,
+                        width: 1,
+                      ),
+                    ),
+                  ),
+                  onPressed: () {
+                    // Navigator.of(context).push(MaterialPageRoute(
+                    //     builder: (context) => const RegisterPage()));
+                  },
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.asset("assets/auth/ic_google.png"),
+                      const SizedBox(width: 10),
+                      const Text(
+                        "Masuk dengan Google",
+                        style: TextStyle(
+                          color: Colors.black,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
       ),
-      
     );
   }
 }
